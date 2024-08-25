d7b58ce6e3afd1748faf2968b03d661e98345e77
ashwani9931 <ashwani89693@gmail.com>
2024-08-25 22:42:19 +0530

    Delete leap_year.cpp

--- leap_year.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int n ,i;
-    cout<<"enter year:"<<endl;
-    cin>>n;
-    string r=(n%4==0 && n%100!=0 ||n%400==0)?"Leap year":"not leap year";
-    cout<<r;
-}
